14df000997e6e39901845b92eb8b5f67fde3efb4
kihangkim <kim.kihang.kiki@gmail.com>
2023-10-11 19:34:21 +0900

    additional controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,17 @@
-import { Body, Controller, Post, Request, UseGuards } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { UsersService } from "../users/users.service";
 import { LocalAuthGuard } from "./guards/local-auth.guard";
 import { RefreshJwtGuard } from "./guards/refresh-jwt-auth.guard";
 import { CreateUserDto } from "../users/dto/create-user.dto";
+import { GoogleGuard } from "./guards/google-auth.guard";
 
 @Controller("auth")
 export class AuthController {
@@ -22,6 +30,12 @@ export class AuthController {
   // async registerUser(@Body() createUserDto: CreateUserDto) {
   //   return await this.userService.create(createUserDto);
   // }
+
+  @Get("google")
+  @UseGuards(GoogleGuard)
+  async googleAuth(): Promise<void> {
+    // redirect google login page
+  }
 
   @UseGuards(RefreshJwtGuard)
   @Post("refresh")
