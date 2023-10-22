5ea002f6c51dea6481687f38440a528ad147a8af
cain <bys20411@gmail.com>
2023-10-22 17:18:46 +0900

    feat: message api response

--- src/modules/messages/message.controller.ts
+++ src/modules/messages/message.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { SendMessageDto } from "./dto/send-message.dto";
 import { MessageService } from "./message.service";
+import { SendMessageResponseDto } from "./dto/send-message-response.dto";
 
 @Controller("messages")
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
   @Post()
-  sendMessages(@Body() { userId }: SendMessageDto) {
-    return this.messageService.sendMessages(userId);
+  async sendMessages(@Body() { userId }: SendMessageDto) {
+    const batchResponse = await this.messageService.sendMessages(userId);
+
+    return new SendMessageResponseDto(batchResponse);
   }
 }
