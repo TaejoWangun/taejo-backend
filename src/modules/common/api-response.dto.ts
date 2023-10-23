c59bfd6b4b46044739f61ff7de96e17cfae1b6c5
cain <bys20411@gmail.com>
2023-10-24 05:46:34 +0900

    feat: api ok response dto

--- src/modules/common/api-response.dto.ts
+++ src/modules/common/api-response.dto.ts
@@ -2,6 +2,7 @@ import { applyDecorators, Type } from "@nestjs/common";
 import {
   ApiCreatedResponse,
   ApiExtraModels,
+  ApiOkResponse,
   getSchemaPath,
 } from "@nestjs/swagger";
 
@@ -9,6 +10,21 @@ export const ApiCustomCreatedResponse = <T extends Type>(dto: T) => {
   return applyDecorators(
     ApiExtraModels(dto),
     ApiCreatedResponse({
+      schema: {
+        allOf: [
+          {
+            $ref: getSchemaPath(dto),
+          },
+        ],
+      },
+    })
+  );
+};
+
+export const ApiCustomOkResponse = <T extends Type>(dto: T) => {
+  return applyDecorators(
+    ApiExtraModels(dto),
+    ApiOkResponse({
       schema: {
         allOf: [
           {
